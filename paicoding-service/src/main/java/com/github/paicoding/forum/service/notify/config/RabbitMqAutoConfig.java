383979ac9373236c335723e8c0b2ef6d244b552d
shiki <2686148374@qq.com>
2026-04-20 18:14:04 +0800

    fist commit

---
+++ paicoding-service/src/main/java/com/github/paicoding/forum/service/notify/config/RabbitMqAutoConfig.java
@@ -0,0 +1,44 @@
+package com.github.paicoding.forum.service.notify.config;
+
+import com.github.paicoding.forum.core.async.AsyncUtil;
+import com.github.paicoding.forum.core.config.RabbitmqProperties;
+import com.github.paicoding.forum.core.rabbitmq.RabbitmqConnectionPool;
+import com.github.paicoding.forum.service.notify.service.RabbitmqService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+/**
+ * @author YiHui
+ * @date 2023/6/9
+ */
+@Configuration
+@ConditionalOnProperty(value = "rabbitmq.switchFlag")
+@EnableConfigurationProperties(RabbitmqProperties.class)
+public class RabbitMqAutoConfig implements ApplicationRunner {
+    @Resource
+    private RabbitmqService rabbitmqService;
+
+    @Resource
+    private RabbitmqProperties rabbitmqProperties;
+
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        //配置连接池
+        String host = rabbitmqProperties.getHost();
+        Integer port = rabbitmqProperties.getPort();
+        String userName = rabbitmqProperties.getUsername();
+        String password = rabbitmqProperties.getPassport();
+        String virtualhost = rabbitmqProperties.getVirtualhost();
+        Integer poolSize = rabbitmqProperties.getPoolSize();
+        RabbitmqConnectionPool.initRabbitmqConnectionPool(host, port, userName, password, virtualhost, poolSize);
+        // 启动消费者
+        AsyncUtil.execute(() -> rabbitmqService.processConsumerMsg());
+    }
+}
